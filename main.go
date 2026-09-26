5f41ccf9f2afb2519149191d0ca608af283584c6
jiggey <70109562+jiggey1@users.noreply.github.com>
2026-09-26 12:36:24 +0100

    fix?

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+// Windows internet API pointers
 var (
 	inetdll  = windows.NewLazySystemDLL("wininet.dll")
 	intopen  = inetdll.NewProc("InternetOpenA")
@@ -19,44 +20,93 @@ var (
 )
 
 func main() {
-	fmt.Println("Spoofing Opera task...")
+	fmt.Println("Opera Spoofer - by skipped.lol")
+	fmt.Println("@jiggey on Discord")
+	time.Sleep(1 * time.Second)
 
+	fmt.Println("\n\nSending request #1")
+
+	// build first req (should work for most people)
 	ua := "Opera Installer/1.0\x00"
 	host := "work.ink\x00"
 	path := "/_api/v2/callback/operaGX\x00"
-	verb := "POST\x00"
+	reqmeth := "POST\x00"
 
-	win, _, _ := intopen.Call(uintptr(unsafe.Pointer(&[]byte(ua)[0])), 1, 0, 0, 0)
+	headers := "Content-Type: application/json\r\nCache-Control: no-cache\r\n\x00"
+	body := `{"noteligible":true}`
+
+	req1bod := doReq(ua, host, path, reqmeth, headers, body)
+
+	fmt.Println("Request 1 Response:", req1bod)
+
+	// build second req (force req)
+	fmt.Println("\n\nSending request #2")
+
+	host = "gig.workink.click\x00"
+	path = "/v2/callback/operaGX?noteligible=1\x00"
+	headers = "Accept: */*\r\nConnection: keep-alive\r\n\x00"
+	reqmeth = "GET\x00"
+
+	req2bod := doReq(ua, host, path, reqmeth, headers, "")
+	fmt.Println("Request 2 Response:", req2bod)
+
+	// build third req (force req)
+	fmt.Println("\n\nSending request #3")
+
+	host = "gig.workink.click\x00"
+	path = "/v2/callback/operaGX\x00"
+	headers = "Accept: */*\r\nConnection: keep-alive\r\n\x00"
+	reqmeth = "GET\x00"
+
+	req3bod := doReq(ua, host, path, reqmeth, headers, "")
+	fmt.Println("Request 3 Response:", req3bod)
+
+	fmt.Println("Finished! Refresh the work.ink page. This window will close in 5 seconds.")
+	fmt.Println("If the work.ink page has not changed, please refresh and try again.")
+	time.Sleep(5 * time.Second)
+}
+
+func doReq(ua string, host string, path string, reqmeth string, headers string, body string) string {
+	uaBytes := []byte(ua)
+	hostBytes := []byte(host)
+	pathBytes := []byte(path)
+	methodBytes := []byte(reqmeth)
+	headerBytes := []byte(headers)
+	bodyBytes := []byte(body)
+
+	win, _, _ := intopen.Call(uintptr(unsafe.Pointer(&uaBytes[0])), 1, 0, 0, 0)
 	if win == 0 {
 		fmt.Println("failed.")
-		return
+		return ""
 	}
 	defer closeint.Call(win)
 
-	connect, _, _ := incon.Call(win, uintptr(unsafe.Pointer(&[]byte(host)[0])), 443, 0, 0, 3, 0, 0)
+	connect, _, _ := incon.Call(win, uintptr(unsafe.Pointer(&hostBytes[0])), 443, 0, 0, 3, 0, 0)
 	if connect == 0 {
 		fmt.Println("failed.")
-		return
+		return ""
 	}
 	defer closeint.Call(connect)
 
 	var dwFlags uintptr = 0x80800000
 
-	reqwest, _, _ := openreq.Call(connect, uintptr(unsafe.Pointer(&[]byte(verb)[0])), uintptr(unsafe.Pointer(&[]byte(path)[0])), 0, 0, 0, dwFlags, 0)
+	reqwest, _, _ := openreq.Call(connect, uintptr(unsafe.Pointer(&methodBytes[0])), uintptr(unsafe.Pointer(&pathBytes[0])), 0, 0, 0, dwFlags, 0)
 	if reqwest == 0 {
-		fmt.Println("Failed to open HTTP request")
-		return
+		fmt.Println("failed.")
+		return ""
 	}
 	defer closeint.Call(reqwest)
 
-	headers := "Content-Type: application/json\r\nCache-Control: no-cache\r\n\x00"
-	body := `{"noteligible":false}`
+	var bodyPtr uintptr
+	if len(bodyBytes) > 0 {
+		bodyPtr = uintptr(unsafe.Pointer(&bodyBytes[0]))
+	}
 
-	success, _, _ := sendreq.Call(reqwest, uintptr(unsafe.Pointer(&[]byte(headers)[0])), uintptr(len(headers)-1), uintptr(unsafe.Pointer(&[]byte(body)[0])), uintptr(len(body)))
+	success, _, _ := sendreq.Call(reqwest, uintptr(unsafe.Pointer(&headerBytes[0])), uintptr(len(headerBytes)-1), bodyPtr, uintptr(len(bodyBytes)))
 
 	if success == 0 {
 		fmt.Println("req failed.")
-		return
+		return ""
 	}
 
 	var buf [4096]byte
@@ -71,8 +121,5 @@ func main() {
 		finalBody += string(buf[:bytesRead])
 	}
 
-	fmt.Println("Response:", finalBody)
-
-	fmt.Println("Finished! Refresh the work.ink page. This window will close in 5 seconds.")
-	time.Sleep(5 * time.Second)
+	return finalBody
 }
